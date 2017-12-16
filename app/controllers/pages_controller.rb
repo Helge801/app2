f9901107bff867397bdb0cdea03845dcb505c80a
Bryan Lund <bryan.helge@gmail.com>
2017-12-16 16:51:45 -0700

    added admin toggle

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,5 +9,18 @@ class PagesController < ApplicationController
   end
 
   def users
+    @users = User.all
   end
+
+  def upgrade
+    @user = User.find(params[:id])
+    if @user.has_roles?(:admin)
+      @user.update(roles: [:user])
+    else
+      @user.update(roles: [:admin])
+    end
+
+    redirect_to pages_users_path
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
 
   get 'pages/users'
 
+  get 'upgrade/:id', as: 'upgrade', to: 'pages#upgrade'
+
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
